0247ad482dfa411e26039a4355cf7d1824a12f86
Kirill <khvorikov.k@yandex.ru>
2021-10-04 11:00:50 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+AOS.init();
+
+
+const swiper = new Swiper('.swiper', {
+    // spaceBetween: 100,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
